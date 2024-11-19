e9c57e92aaae03c6100f05de8debb3c313d37cdc
Stefany Junges <stefanyrjunges@gmail.com>
2024-11-19 18:26:02 +0000

    Commit project

---
+++ src/hydrationtrackerapp/HydrationTrackerApp.java
@@ -0,0 +1,18 @@
+/*
+* HydrationTrackerApp.java
+* @author: Stefany Rachor Junges
+* 09/11/2024
+*/
+
+package hydrationtrackerapp;
+
+public class HydrationTrackerApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
